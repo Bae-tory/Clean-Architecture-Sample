110abb8d260178733c456c03d8f437dbdf651bdf
Sungjae <dunkey2615@gmail.com>
2020-08-26 13:56:28 +0900

    ConfirmDialog.kt 삭제

--- presentation/src/main/java/com/sungjae/portfolio/ui/search/ConfirmDialog.kt
+++ presentation/src/main/java/com/sungjae/portfolio/ui/search/ConfirmDialog.kt
@@ -1,21 +1,2 @@
 package com.sungjae.portfolio.ui.search
 
-import android.content.Context
-import com.sungjae.portfolio.R
-import com.sungjae.portfolio.components.BaseDialogHelper
-import com.sungjae.portfolio.databinding.DialogConfirmBinding
-
-class ConfirmDialog(
-    context: Context,
-    override val vm: ContentViewModel
-) : BaseDialogHelper<ConfirmDialog, DialogConfirmBinding, ContentViewModel>(R.layout.dialog_confirm, context) {
-
-    override fun show() {
-
-    }
-
-    override fun dismiss() {
-        vm.unBindViewModel()
-        super.dismiss()
-    }
-}
