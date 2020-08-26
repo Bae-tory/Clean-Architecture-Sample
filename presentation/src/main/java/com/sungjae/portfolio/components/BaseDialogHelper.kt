1488d8bb64b57cca9fbb3ca36471c717636735f3
Sungjae <dunkey2615@gmail.com>
2020-08-26 12:45:24 +0900

    베이스 다이얼로그 헬퍼 수정

--- presentation/src/main/java/com/sungjae/portfolio/components/BaseDialogHelper.kt
+++ presentation/src/main/java/com/sungjae/portfolio/components/BaseDialogHelper.kt
@@ -3,32 +3,27 @@ package com.sungjae.portfolio.components
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import com.sungjae.portfolio.BR
-import com.sungjae.portfolio.R
 import com.sungjae.portfolio.base.BaseViewModel
 
 
-abstract class BaseDialogHelper<Dialog : BaseDialogHelper<Dialog, B, VM>, B : ViewDataBinding, VM : BaseViewModel>(
-    @LayoutRes private val dialogLayoutRes: Int = 0,
+abstract class BaseDialogHelper<B : ViewDataBinding, VM : BaseViewModel>(
+    @LayoutRes private val dialogLayoutRes: Int,
     private val context: Context
 ) : LifecycleOwner {
 
-    protected lateinit var binding: B
+    protected var binding: B? = null
         private set
     protected abstract val vm: VM
-
-    private val lifecycleRegistry by lazy {
-        LifecycleRegistry(this)
-    }
+    private val lifecycleRegistry by lazy { LifecycleRegistry(this) }
 
     open var bottomAnim = false
     open var cancelable: Boolean = true
@@ -36,6 +31,11 @@ abstract class BaseDialogHelper<Dialog : BaseDialogHelper<Dialog, B, VM>, B : Vi
     open var isBackgroundTransparent: Boolean = true
     open var totalSize = false
     private var dialog: AlertDialog? = null
+
+    fun initDialog() {
+        setBinding()
+        setDialog()
+    }
 
     private fun setBinding() {
         binding = DataBindingUtil.inflate(LayoutInflater.from(context), dialogLayoutRes, null, false)
@@ -46,30 +46,30 @@ abstract class BaseDialogHelper<Dialog : BaseDialogHelper<Dialog, B, VM>, B : Vi
     }
 
     private fun setDialog() {
-        dialog = AlertDialog.Builder(context).setView(binding.root).setCancelable(cancelable).create()
+        dialog = AlertDialog.Builder(context).setView(binding?.root).setCancelable(cancelable).create()
         val bgRes = if (isBackgroundTransparent) android.R.color.transparent else android.R.color.black
         dialog?.window?.setBackgroundDrawableResource(bgRes)
+//        if (bottomAnim) dialog?.window?.attributes?.windowAnimations = R.style.dialogAnim
     }
 
     protected fun bind(action: B.() -> Unit) {
-        binding.run(action)
+        binding?.run(action)
     }
 
-    open fun show() {
-        setBinding()
-        setDialog()
+    protected fun show() {
+        dialog?.show()
 
         val windows = dialog?.window
         if (totalSize) {
-            windows?.setLayout(MATCH_PARENT, MATCH_PARENT)
+            windows?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
         } else {
-            windows?.setLayout(MATCH_PARENT, WRAP_CONTENT)
+            windows?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
         }
         if (gravity != Gravity.CENTER) {
             dialog?.window?.setGravity(gravity)
         }
-        dialog?.show()
     }
+
 
     open fun dismiss() {
         if (isShowing() == true) dialog?.dismiss()
