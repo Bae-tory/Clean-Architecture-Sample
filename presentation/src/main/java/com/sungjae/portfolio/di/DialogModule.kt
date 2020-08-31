8a79eca901ce9e5c9afadb1354737b5ae3f82e57
Sungjae <dunkey2615@gmail.com>
2020-08-31 16:45:00 +0900

    DialogModule.kt 주석처리

--- presentation/src/main/java/com/sungjae/portfolio/di/DialogModule.kt
+++ presentation/src/main/java/com/sungjae/portfolio/di/DialogModule.kt
@@ -1,10 +1,9 @@
 package com.sungjae.portfolio.di
 
 import android.content.Context
-import com.sungjae.portfolio.ui.search.ConfirmDialog
 import org.koin.dsl.module
 
 val dialogModule = module {
 
-    factory { (context: Context) -> ConfirmDialog(context, get()) }
+//    factory { (context: Context) -> ConfirmDialog(context, get()) }
 }
