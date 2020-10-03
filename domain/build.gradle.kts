f1b5981abae2eac11e635caa26340d4fa0bf77f3
developer-sungjae <dunkey2615@naver.com>
2020-10-03 23:35:03 +0900

    Fix : Domain, Coroutine Version

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,55 +1,18 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    kotlin("android.extensions")
-    kotlin("kapt")
-}
-
-android {
-    compileSdkVersion(Apps.COMPILE_SDK)
-    buildToolsVersion(Apps.BUILD_TOOL_VERSION)
-
-    defaultConfig {
-        minSdkVersion(Apps.MIN_SDK)
-        targetSdkVersion(Apps.TARGET_SDK)
-        versionCode = Apps.VERSION_CODE
-        versionName = Apps.VERSION_NAME
-    }
-
-    buildTypes {
-        getByName(BuildTypes.RELEASE) {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = Apps.sourceCompatibility
-        targetCompatibility = Apps.targetCompatibility
-    }
-
-    sourceSets {
-        getByName(SourceSets.MAIN).java.srcDir("src/main/kotlin")
-        getByName(SourceSets.TEST).java.srcDir("src/test/kotlin")
-    }
-
-    kotlinOptions {
-        jvmTarget = Apps.JVM_TARGET
-    }
+    `java-library`
+    id("kotlin")
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(Libs.KOTLIN)
     implementation(Libs.CORE_KTX)
     implementation(Libs.APP_COMPAT)
-    implementation(Libs.RX_JAVA2)
+
     implementation(Libs.RX_ANDROID)
     implementation(Libs.RX_KOTLIN)
 
-    implementation(Libs.CONVERTER_GSON)
+    //DI
+    implementation(Libs.KOIN_VIEWMODEL)
 
-    //Test
-    implementation(TestLibs.JUNIT)
-    implementation(TestLibs.JUNIT_EXT)
-    implementation(TestLibs.ESPRESSO)
+    implementation(Libs.CONVERTER_GSON)
 }
