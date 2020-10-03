bd17b2c21d722be0939191f51b84d8e8b61205a7
developer-sungjae <dunkey2615@naver.com>
2020-10-03 23:37:14 +0900

    Fix : Delete unused import

--- domain/src/main/java/com/sungjae/portfolio/domain/usecase/LoadContentByHistoryUseCase.kt
+++ domain/src/main/java/com/sungjae/portfolio/domain/usecase/LoadContentByHistoryUseCase.kt
@@ -5,11 +5,6 @@ import com.sungjae.portfolio.domain.exception.InvalidTabTypeException
 import com.sungjae.portfolio.domain.exception.Result
 import com.sungjae.portfolio.domain.repository.Repository
 import com.sungjae.portfolio.domain.usecase.base.ResultInputUseCase
-import com.sungjae.portfolio.domain.usecase.base.SingleInputUseCase
-import io.reactivex.Scheduler
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class LoadContentByHistoryUseCase(
     private val repository: Repository
